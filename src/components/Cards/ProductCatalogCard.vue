4fb8d0c399bba58d82b46e6434296146e2008a38
Raendy Andhika El-Vicar <raendy.elvicar@gmail.com>
2022-06-13 14:59:15 +0700

    adjust product image

--- src/components/Cards/ProductCatalogCard.vue
+++ src/components/Cards/ProductCatalogCard.vue
@@ -1,21 +1,26 @@
 <template lang="">
   <div class="product-catalog-card">
-    <div class="card-header">
+    <div class="card-header w-full">
       <img :src="imgUrl" />
     </div>
-    <div class="card-body">
-      <p class="product-title text-sm">{{ title }}</p>
-      <p class="product-price text-sm font-bold">{{ price }}</p>
+    <div class="wrapper">
+      <div class="card-body">
+        <p class="product-title text-sm w-full">{{ title }}</p>
+        <p class="product-price text-sm font-bold">{{ price }}</p>
+      </div>
+      <Button
+        cta="Keranjang"
+        class="bucket flex justify-center items-center w-full"
+      >
+        <Plus
+          height="12px"
+          width="12px"
+          color="#015CA1"
+          slot="icon"
+          style="margin-right: 5px"
+        ></Plus>
+      </Button>
     </div>
-    <Button cta="Keranjang" class="bucket flex justify-center items-center">
-      <Plus
-        height="12px"
-        width="12px"
-        color="#015CA1"
-        slot="icon"
-        style="margin-right: 5px"
-      ></Plus>
-    </Button>
   </div>
 </template>
 <script>
@@ -42,23 +47,24 @@ export default {
   margin: 20px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-  padding: 10px;
   border-radius: 10px;
   transition: all 0.2s ease-in-out 0s;
   cursor: pointer;
 }
+
 .product-catalog-card:hover {
   transform: translateY(-5px);
 }
 
-.card-header {
-  width: 100%;
-}
-
 .card-header img {
+  border-radius: 10px 10px 0 0;
   max-width: 100%;
   max-height: 100%;
   object-fit: cover;
+}
+
+.wrapper {
+  padding: 10px;
 }
 
 .card-body {
@@ -66,7 +72,6 @@ export default {
   justify-content: space-between;
   flex-direction: column;
   align-items: flex-start;
-  margin: 10px 0;
 }
 
 .card-body p {
@@ -74,7 +79,6 @@ export default {
 }
 
 .card-body .product-title {
-  width: 100%;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
